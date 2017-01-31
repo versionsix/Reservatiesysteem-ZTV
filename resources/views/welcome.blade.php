cc0368d22fac25854b888829aa0c87b18c233190
Unknown <frankmeeusen@gmail.com>
2017-01-31 15:33:29 +0100

    Stapje verder: nieuwe groetmessage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Let's go with the tickets
                 </div>
 
                 <div class="links">
